c559c08961e3b6127f972fa25a7e5fcfa71cd12c
Mike Semcheski <mhs@Mikes-MBP.localdomain>
2021-02-14 12:30:35 -0500

    WIP

--- main.go
+++ main.go
@@ -2,16 +2,70 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"path"
 	"strings"
+
+	"gopkg.in/yaml.v2"
 )
 
+var data = Flashcards{
+	PageTitle: "LE Card",
+	Cards: []Card{
+		{"vcle", []string{"bridle", "cable", "able", "cradle", "rifle", "table", "bugle"}},
+		{"vccle", []string{"jungle", "scramble", "handle", "tremble", "sample", "single", "muscle"}},
+		{"vccle2", []string{"rattle", "bottle", "middle", "paddle", "battle", "scribble", "settle"}},
+	},
+}
+
+var data1 = Flashcards{
+	PageTitle: "L Card",
+	Cards: []Card{
+		{"le", []string{"angle", "struggle", "saddle", "couple", "needle", "bundle"}},
+		{"el", []string{"cancel", "level", "vowel", "novel", "angel", "jewel"}},
+		{"il", []string{"council", "stencil", "evil", "pupil", "fossil", "April"}},
+		{"al", []string{"pedal", "total", "metal", "local", "journal", "signal"}},
+		{"oddball", []string{"special", "fragile"}},
+	},
+}
+
 func main() {
+
 	pr := newPathResolver()
 	pr.Add("GET /hello", hello)
-	pr.Add("* /goodbye/*", goodbye)
+	pr.Add("GET /goodbye", goodbye)
+	pr.Add("GET /list", wip)
+	pr.Add("GET /slideshow", slideshow)
+	pr.Add("* *", generic)
+
+	fmt.Fprint(os.Stdout, data)
+	fmt.Fprint(os.Stdout, data1)
+
 	http.ListenAndServe(":8080", pr)
+
+}
+
+func generic(res http.ResponseWriter, req *http.Request) {
+
+	path := req.URL.Path
+	parts := strings.Split(path, "/")
+
+	file := parts[1]
+	//view := parts[2]
+
+	filecontents, err := ioutil.ReadFile(file + ".yml")
+	if err != nil {
+		res.WriteHeader(500)
+	}
+
+	type Data struct{}
+	d := Data{}
+	err = yaml.Unmarshal(filecontents, d)
+	b, err := yaml.Marshal(d)
+	res.Write(b)
 
 }
 
@@ -39,13 +93,71 @@ func (p *pathResolver) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request){
-	query := req.URL.Query()
-	name := query.Get("name")
-	if name == "" {
-		name = "Mike"
+func wip(res http.ResponseWriter, req *http.Request) {
+
+	t := FlashcardSet{[]Flashcards{data, data1}}
+
+	d, err := yaml.Marshal(t)
+	if err != nil {
+		panic(err)
 	}
-	fmt.Fprint(res, "Hej, han heter ", name)
+	res.Write(d)
+}
+
+type Card struct {
+	Name string
+	Word []string
+}
+
+type Flashcards struct {
+	PageTitle string
+	Cards     []Card
+}
+
+type FlashcardSet struct {
+	Flashcards []Flashcards
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+
+	data, data1 := flashcard()
+
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+
+	tmpl.Execute(res, data)
+	tmpl.Execute(res, data1)
+
+}
+
+func flashcard() (Flashcards, Flashcards) {
+	data := Flashcards{
+		PageTitle: "LE Card",
+		Cards: []Card{
+			{"vcle", []string{"bridle", "cable", "able", "cradle", "rifle", "table", "bugle"}},
+			{"vccle", []string{"jungle", "scramble", "handle", "tremble", "sample", "single", "muscle"}},
+			{"vccle2", []string{"rattle", "bottle", "middle", "paddle", "battle", "scribble", "settle"}},
+		},
+	}
+
+	data1 := Flashcards{
+		PageTitle: "L Card",
+		Cards: []Card{
+			{"le", []string{"angle", "struggle", "saddle", "couple", "needle", "bundle"}},
+			{"el", []string{"cancel", "level", "vowel", "novel", "angel", "jewel"}},
+			{"il", []string{"council", "stencil", "evil", "pupil", "fossil", "April"}},
+			{"al", []string{"pedal", "total", "metal", "local", "journal", "signal"}},
+			{"oddball", []string{"special", "fragile"}},
+		},
+	}
+	return data, data1
+}
+
+func slideshow(res http.ResponseWriter, req *http.Request) {
+
+	flashcards := FlashcardSet{[]Flashcards{data, data1}}
+	tmpl := template.Must(template.ParseFiles("slideshow.html"))
+
+	tmpl.Execute(res, flashcards)
 
 }
 
@@ -54,8 +166,10 @@ func goodbye(res http.ResponseWriter, req *http.Request) {
 	parts := strings.Split(path, "/")
 	name := parts[2]
 	if name == "" {
-		name = "Mike"
+		name = "Monika"
 	}
-	fmt.Fprint(res, "Hej Då", name)
+
+	fmt.Fprint(res, "Hej Då ", name)
+	fmt.Fprint(res, ".")
 
 }
